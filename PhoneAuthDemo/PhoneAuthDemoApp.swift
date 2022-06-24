a4bc4695c4fabaaa825f659012b88431af565827
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2022-06-24 09:31:09 +0300

    Initial Commit

---
+++ PhoneAuthDemo/PhoneAuthDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneAuthDemoApp.swift
+//  PhoneAuthDemo
+//
+//  Created by khawlah khalid on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhoneAuthDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
