e089919d51331c4140765058c5df94a66389eee4
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2022-06-25 11:11:10 +0300

    Keep user logged in ✌🏻

--- PhoneAuthDemo/AuthViewModel.swift
+++ PhoneAuthDemo/AuthViewModel.swift
@@ -8,18 +8,23 @@
 import Foundation
 import Firebase
 
-
 final class AuthViewModel : ObservableObject{
+    @Published var userSession : Firebase.User?
     @Published var verificationId : String? //the code that is sended by Firebase
     @Published var phoneNumber : String = ""
     @Published var smsCode: String = ""
-    @Published var isShowingHomeView: Bool = false
+    @Published var isAouthenticatting: Bool = false
     let auth = Auth.auth()
-    
+    init(){
+        userSession = Auth.auth().currentUser
+        if let _ = userSession {
+            isAouthenticatting = true
+        }
+    }
     
     func createAccountWithPhoneNumber(){
         let phoneNumber = "+966"+self.phoneNumber
-        print(phoneNumber,"🔴")
+        //Will send the SMS message to the given phone number (in case no errors), and will return the same code in verificationId
         PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { verificationId, error in
             if let error = error {
                 print("Error while getting sms : \(error)")
@@ -42,9 +47,8 @@ final class AuthViewModel : ObservableObject{
                     return
                 }
                 
-                print("DEBUG: 6 - Successfully Uploaded user info to firebase with saved phone number")
+                print("DEBUG: Successfully Uploaded user info to firebase with saved phone number")
                             guard let result = result else {
-                                print("here")
                                 return}
                             let userId = result.user.uid
                             let userData = [
@@ -53,7 +57,7 @@ final class AuthViewModel : ObservableObject{
                             ]
                             
                             Firestore.firestore().collection("users").document(userId).setData(userData)
-                            self.isShowingHomeView.toggle()
+                            self.isAouthenticatting.toggle()
             }
         }
     }
--- PhoneAuthDemo/ContentView.swift
+++ PhoneAuthDemo/ContentView.swift
@@ -9,47 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var authViewModel = AuthViewModel()
-    
     var body: some View {
         NavigationView{
-            VStack{
-                TextField("Enter Phone Number", text: $authViewModel.phoneNumber)
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.white, lineWidth: 0.5)
-                    )
-                    .padding()
-                    .keyboardType(.numberPad)
-                Button {
-                    authViewModel.createAccountWithPhoneNumber()
-                } label: {
-                    Text("Create Account")
-                }
-                
-                Divider()
-                
-                TextField("Enter OPT Code", text: $authViewModel.smsCode)
-                    .padding()
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(Color.white, lineWidth: 0.5)
-                    )
-                    .padding()
-                    .keyboardType(.numberPad)
-                
-                Button {
-                       authViewModel.verifySMSCode()
-                } label: {
-                    Text("Verify Code")
-                }
-                
-                
-                
-                .navigationTitle("Phone Auth")
+            if authViewModel.isAouthenticatting{
+                Text("Welcome 💜")
             }
-            .fullScreenCover(isPresented: $authViewModel.isShowingHomeView) {
-                Text("You are Good to go 😎")
+            else{
+                VStack{
+                    TextField("Phone Number", text: $authViewModel.phoneNumber)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.white, lineWidth: 0.5)
+                        )
+                        .padding()
+                        .keyboardType(.numberPad)
+                    Button {
+                        authViewModel.createAccountWithPhoneNumber()
+                    } label: {
+                        Text("Create Account")
+                    }
+                    
+                    Divider()
+                    
+                    TextField("Enter OPT Code", text: $authViewModel.smsCode)
+                        .padding()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.white, lineWidth: 0.5)
+                        )
+                        .padding()
+                        .keyboardType(.numberPad)
+                    
+                    Button {
+                           authViewModel.verifySMSCode()
+                    } label: {
+                        Text("Verify Code")
+                    }
+                    .navigationTitle("Phone Auth")
+                }
             }
         }
     }
